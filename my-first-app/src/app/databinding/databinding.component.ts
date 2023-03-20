ae083e2c5dae8d8c4d80b79874ef2c2d3801b59c
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-03-20 09:40:08 +0530

    update

--- my-first-app/src/app/databinding/databinding.component.ts
+++ my-first-app/src/app/databinding/databinding.component.ts
@@ -3,7 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-databinding',
   templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  //styleUrls: ['./databinding.component.css']
+  styles: [`
+  p{
+    font-size: 12rm;
+    color: brown;
+    background-color: bisque;
+}
+  `
+  ]
 })
 export class DatabindingComponent implements OnInit {
 
