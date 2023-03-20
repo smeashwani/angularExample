3b96f76e85b5a7a69d0e7f6c744b0217d349909b
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-03-20 16:24:22 +0530

    update

---
+++ directive-example/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directive-example';
+  employeedata:any;
+  employee = [{
+    empid: 5623,
+    fullname: "Stephen",
+    city: "Newyork",
+    experience: "5 years",
+    salary: 500000,
+    married: false,
+    role: 'admin'
+  },
+  {
+    empid: 6523,
+    fullname: "Michel",
+    city: "london",
+    experience: "7 years",
+    salary: 700000,
+    married: true,
+    role: 'user'
+  },
+  {
+    empid: 6523,
+    fullname: "peeter",
+    city: "Dubai",
+    experience: "8 years",
+   // salary: null,
+    married: true,
+    role: 'public'
+  },
+  {
+    empid: 7458,
+    fullname: "stella",
+    city: "Paris",
+    experience: "4 years",
+    salary: 400000,
+    married: false,
+    role: 'user'
+  }];
+}
