1d4c389a43fa58bc7237df194ff6a1d4b797d88a
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-03-21 15:58:44 +0530

    update

--- cmp-binding/src/app/server-element/server-element.component.ts
+++ cmp-binding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
- element :any = {name:'', type:'', content:''};
+ @Input() element :any = {name:'', type:'', content:''};
   constructor() { }
 
   ngOnInit(): void {
