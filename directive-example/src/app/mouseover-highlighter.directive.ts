2cd8917ea067993d8640558833ebdc6f4e4bae29
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-03-21 20:25:00 +0530

    Update mouseover-highlighter.directive.ts

--- directive-example/src/app/mouseover-highlighter.directive.ts
+++ directive-example/src/app/mouseover-highlighter.directive.ts
@@ -1,22 +1,20 @@
-import { AfterViewInit, Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appMouseoverHighlighter]'
 })
 export class MouseoverHighlighterDirective implements AfterViewInit{
-
+  @HostBinding('style.fontSize') fontsize: string = 'inherit';
   constructor(private elRef: ElementRef, private redderer: Renderer2) { }
   
   @HostListener('mouseenter')
-  mouseover(enentData: Event){
-    let element = this.elRef.nativeElement;
-    this.redderer.setStyle(element, 'font-size', 'large');
+  mouseover(){
+    this.fontsize ='large';
   }
 
   @HostListener('mouseleave')
-  mouseleave(enentData: Event){
-    let element = this.elRef.nativeElement;
-    this.redderer.setStyle(element, 'font-size', 'inherit');
+  mouseleave(){
+    this.fontsize ='inherit';
   }
 
   ngAfterViewInit(): void {
