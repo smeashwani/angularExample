43ffd1668035abaeb61a1a68e9509ced377716c4
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-03-21 16:59:11 +0530

    update

--- cmp-binding/src/app/app.component.ts
+++ cmp-binding/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements : any = [{name:'Guest', type:'server',content:'Defualt value'}];
+  onFetchingData(fetchData: {serverName: string, serverType: string,  serverContent:string}){
+    this.serverElements.push({
+      name: fetchData.serverName,
+      type: fetchData.serverType,
+      content: fetchData.serverContent
+    });
+  }
 }
